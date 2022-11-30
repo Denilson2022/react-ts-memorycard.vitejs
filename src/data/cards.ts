97b6905bb1dc9d3939ff2be5d260874ecae5e397
Denilson <denilson_email@yahoo.com.br>
2022-11-29 22:07:46 -0300

    alterando o caminho das imagens dos cards

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -1,15 +1,15 @@
 export const cards = [
   {
     id: '1',
-    back: '../../../public/images/ni.jpg'
+    back: 'https://res.cloudinary.com/diootq9ps/image/upload/v1669768723/pasta/ni_wrnnmc.jpg'
   }, {
     id: '2',
-    back: '../../../public/images/bia.jpg'
+    back: 'https://res.cloudinary.com/diootq9ps/image/upload/v1669768723/pasta/bia_zezkam.jpg'
   }, {
     id: '3',
-    back: '../../../public/images/hagatha.jpg'
+    back: 'https://res.cloudinary.com/diootq9ps/image/upload/v1669768723/pasta/hagatha_ktlnwv.jpg'
   }, {
     id: '4',
-    back: '../../../public/images/Heitor.jpg'
+    back: 'https://res.cloudinary.com/diootq9ps/image/upload/v1669768723/pasta/Heitor_atalwu.jpg'
   },
 ]
